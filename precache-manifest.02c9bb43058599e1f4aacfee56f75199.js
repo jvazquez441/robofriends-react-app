fb9c89e290dc209e4639c5b052d60c9d4a7770ed
Jonathan <jvazquez441@email.suagm.edu>
2019-01-09 02:59:53 -0400

    Updates

---
+++ precache-manifest.02c9bb43058599e1f4aacfee56f75199.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0c6dadd5d70ba56a0ae8",
+    "url": "/robofriends-react-app/static/js/runtime~main.0c6dadd5.js"
+  },
+  {
+    "revision": "75a830ce4eaa5919b7af",
+    "url": "/robofriends-react-app/static/js/main.75a830ce.chunk.js"
+  },
+  {
+    "revision": "bca851e6590111661a94",
+    "url": "/robofriends-react-app/static/js/1.bca851e6.chunk.js"
+  },
+  {
+    "revision": "75a830ce4eaa5919b7af",
+    "url": "/robofriends-react-app/static/css/main.11c01762.chunk.css"
+  },
+  {
+    "revision": "bca851e6590111661a94",
+    "url": "/robofriends-react-app/static/css/1.36cbfb53.chunk.css"
+  },
+  {
+    "revision": "b997e8923b394cce68027c1da52620bf",
+    "url": "/robofriends-react-app/index.html"
+  }
+];
